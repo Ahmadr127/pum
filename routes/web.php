b89dab7d8bec88e522504232effba0cc0ba734c5
ahmadr127 <ahmdrifai808@gmail.com>
2026-03-11 16:35:29 +0700

    update sso json

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,7 @@ Route::get('/auth/sso/callback', function (Request $request) {
     try {
         $ssoUserResponse = Http::withToken($accessToken)
             ->get(env('SSO_BASE_URL') . '/api/user');
-        $ssoUser = $ssoUserResponse->json();
+        $ssoUser = $ssoUserResponse->json('data') ?? $ssoUserResponse->json();
 
         if (empty($ssoUser['email'])) {
             $errorBody = $ssoUserResponse->body();
