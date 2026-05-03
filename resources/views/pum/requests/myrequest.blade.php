ef132768c3498d5603a37a18d8d54fdb99f80069
ahmadr127 <ahmdrifai808@gmail.com>
2026-05-03 22:35:35 +0700

    feat: create my request view with scan PDF functionality and advanced filtering for PUM requests

--- resources/views/pum/requests/myrequest.blade.php
+++ resources/views/pum/requests/myrequest.blade.php
@@ -13,10 +13,10 @@
                 <i class="fas fa-file-pdf mr-2"></i>
                 Scan PDF
             </button>
-            <a href="{{ route('pum-requests.create') }}" class="hidden bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded items-center text-sm">
+            <!-- <a href="{{ route('pum-requests.create') }}" class="hidden bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded items-center text-sm">
                 <i class="fas fa-plus mr-2"></i>
                 Tambah Pengajuan
-            </a>
+            </a> -->
         </div>
 
         <!-- Filters - Single Row -->
